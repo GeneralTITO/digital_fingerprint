70f66550b98e2b1bc3690fd4a09488f37a3cd465
tito <twiceqbit@gmail.com>
2023-11-08 20:26:54 -0300

    feat: primeira parte notebook e verificação da main ok

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from tkinter import *
 from tkinter import ttk
-from utils.admin import  acessar_parte_restrita
+from utils.admin import acessar_parte_restrita
 from utils.relogio import atualizar_hora
 from utils.fingerprint import *
 
 root = Tk()
-root.title('Fingerprint')
+root.title("Fingerprint")
 
 root.rowconfigure(0, weight=1)
 root.columnconfigure(0, weight=1)
@@ -13,7 +13,9 @@ root.columnconfigure(0, weight=1)
 
 frame_interval_info = ttk.Frame(root)
 frame_interval_info.grid(column=0, row=0, pady=2)
-label_interval_info = ttk.Label(frame_interval_info, text='intervalo de verificação:<<<9:00 - 9:15>>> ', padding=10)
+label_interval_info = ttk.Label(
+    frame_interval_info, text="intervalo de verificação:<<<9:00 - 9:15>>> ", padding=10
+)
 label_interval_info.grid(column=0, row=0)
 
 frame_horario_atual = ttk.Frame(root)
@@ -25,24 +27,43 @@ atualizar_hora(label=label_horario_atual, root=root)
 
 frame_button = ttk.Frame(root)
 frame_button.grid(column=0, row=3, pady=10)
-button_verication = ttk.Button(frame_button, text='Verificar digital', padding=10, command=enroll)
+
+name_var = StringVar()
+
+
+def verify_func():
+    name = verify()
+    if name == "Digital não cadastrada":
+        return name_var.set("Digital não cadastrada")
+    name_var.set(f"{name}, digital verificada")
+
+
+button_verication = ttk.Button(
+    frame_button, text="Verificar digital", padding=10, command=verify_func
+)
 button_verication.grid(column=0, row=0)
-label_verication = ttk.Label(frame_button, text='<<nome>> digital verificada')
+label_verication = ttk.Label(frame_button, textvariable=name_var)
 label_verication.grid(row=2, column=0)
 
-ttk.Separator(root, orient="horizontal").grid(row=4, column=0, sticky="ew", padx=10, pady=10)
+ttk.Separator(root, orient="horizontal").grid(
+    row=4, column=0, sticky="ew", padx=10, pady=10
+)
 
 frame_admin = ttk.Frame(root)
 frame_admin.grid(row=5, column=0, padx=10, pady=10)
-label_admin = ttk.Label(frame_admin, text='Admin: ')
+label_admin = ttk.Label(frame_admin, text="Admin: ")
 label_admin.grid(column=0, row=0)
 senha = StringVar()
-entry_admin = ttk.Entry(frame_admin, textvariable=senha, show='*')
+entry_admin = ttk.Entry(frame_admin, textvariable=senha, show="*")
 entry_admin.grid(column=2, row=0, padx=10, pady=10)
+
+
 def access_buttun_fuc():
     acessar_parte_restrita(senha)
-    senha.set('')
-button_access = ttk.Button(frame_admin, text='Acessar', command=access_buttun_fuc)
+    senha.set("")
+
+
+button_access = ttk.Button(frame_admin, text="Acessar", command=access_buttun_fuc)
 button_access.grid(row=0, column=3)
 
 
--- utils/fingerprint.py
+++ utils/fingerprint.py
@@ -1,6 +1,8 @@
 import nbsp_python as nbsp
+from utils.handle_data import carregar_pessoas
 
-def enroll ():
+
+def enroll():
     try:
         nbsp.initialize_handle()
         nbsp.open_device()
@@ -12,10 +14,27 @@ def enroll ():
     nbsp.terminate_handle()
     return fingerprint_hash
 
-def match (hash_stored):
+
+def match(hash_stored):
     handle = nbsp.capture("verify", 10000)
     fingerprint_hash = nbsp.extract_fir_text(handle)
-    if nbsp.match(hash_stored,fingerprint_hash):
-        print('ok')
+    if nbsp.match(hash_stored, fingerprint_hash):
+        return True
     else:
-        print('not ok')
+        return False
+
+
+def verify():
+    try:
+        nbsp.initialize_handle()
+        nbsp.open_device()
+    except:
+        pass
+
+    handle = nbsp.capture("verify", 10000)
+    fingerprint_hash = nbsp.extract_fir_text(handle)
+    pessoas = carregar_pessoas()
+    for pessoa in pessoas:
+        if nbsp.match(pessoa[0], fingerprint_hash):
+            return pessoa[1]
+    return "Digital não cadastrada"
